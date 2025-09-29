52cf2acf953d9ef94e5e8428ff1b73067efa5ea8
acvalenzuela <anne.christine.valenzuela@student.laurea.fi>
2025-09-29 22:51:52 +0300

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TinsAppLab-2',
+  webDir: 'dist'
+};
+
+export default config;
